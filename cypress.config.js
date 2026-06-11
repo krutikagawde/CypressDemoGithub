ee7ef4c14f8479663f4e13ac19838815bea47749
Test <krutika.gawde@gmail.com>
2026-06-11 09:44:12 -0500

    Connected Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
+projectId: "g3pqvi",
   e2e: {
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
